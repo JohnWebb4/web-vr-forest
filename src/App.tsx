8861e8b12ed5eab8bdbeec742344193902bf4d04
John Webb <John.Webb94@gmail.com>
2018-12-21 16:02:00 -0700

    Arrange trees in random circle around origin

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { themes } from "./styles/themes";
 
-import { loadAssets as loadForest, render as renderForest } from "./environments/forest";
+import { Forest, LoadForest } from "./environments/forest";
 
 function App() {
   const theme = new themes.MainTheme();
 
+  const [numTrees, updateNumTrees] = useState(150);
+  const [minDistance, updateMinDistance ] = useState(1.5);
+  const [maxDistance, updateMaxDistance ] = useState(6);
+
   return (
     <a-scene>
       <a-assets>
-        {loadForest()}
+        <LoadForest />
       </a-assets>
       <a-entity position="0 0 0" rotation="0 0 0">
         <a-camera near="0.1" user-height="0">
         </a-camera>
       </a-entity>
-      {renderForest(theme)}
+      <Forest theme={theme} numTrees={numTrees} minDistance={minDistance} maxDistance={maxDistance} />
     </a-scene>
   );
 }
--- src/environments/forest.tsx
+++ src/environments/forest.tsx
@@ -1,12 +1,20 @@
-import { cloneDeep } from "lodash";
-import React, { Fragment, useEffect, useState } from "react";
+import { cloneDeep, isEqual } from "lodash";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 
 import { ITheme } from "../typings/itheme";
 import { getRandomHexBetweenValues } from "../utils/colors.util";
+import { getRandomInRange } from "../utils/math.util";
 
 import { IProps as TreeProps, Tree } from "../components/tree";
 
-function loadAssets(): JSX.Element {
+interface IForestProps {
+  maxDistance: number;
+  minDistance: number;
+  numTrees: number;
+  theme: ITheme;
+}
+
+function LoadForest(): JSX.Element {
     return (
       <Fragment>
         <img id="tree1-bark" src="images/tree1-bark.png"></img>
@@ -15,21 +23,32 @@ function loadAssets(): JSX.Element {
     );
   }
 
-function render(theme: ITheme): JSX.Element {
-  const [trees, updateTrees] = useState<TreeProps[]>([
-    {
-      position: "0 0 -2",
-    },
-  ]);
+function Forest({maxDistance, minDistance, numTrees, theme}: IForestProps): JSX.Element {
+  const [trees, updateTrees] = useState<TreeProps[]>(generateTrees(theme, numTrees, minDistance, maxDistance));
+
+  const previousTheme = usePreviousValue(theme);
+  const previousEnvironmentProps = usePreviousValue({
+    maxDistance,
+    minDistance,
+    numTrees,
+  });
 
   useEffect(() => {
-    const newTrees: TreeProps[] = cloneDeep(trees).map((tree) => ({
-      ...tree,
-      barkColor: theme.tertiaryColor,
-      leafColor: getRandomHexBetweenValues(theme.primaryColor, theme.secondaryColor),
-    }));
+    if (!isEqual(theme, previousTheme)) {
+      const newTrees: TreeProps[] = cloneDeep(trees).map((tree) => ({
+        ...tree,
+        barkColor: theme.tertiaryColor,
+        leafColor: getRandomHexBetweenValues(theme.primaryColor, theme.secondaryColor),
+      }));
 
-    updateTrees(newTrees);
+      updateTrees(newTrees);
+    }
+  });
+
+  useEffect(() => {
+    if (!isEqual({ maxDistance, minDistance, numTrees}, previousEnvironmentProps)) {
+      updateTrees(generateTrees(theme, numTrees, minDistance, maxDistance));
+    }
   });
 
   return (
@@ -44,4 +63,45 @@ function render(theme: ITheme): JSX.Element {
   );
 }
 
-export { loadAssets, render };
+function generateTrees(theme: ITheme, numTrees: number, minDistance: number, maxDistance: number): TreeProps[] {
+  const trees: TreeProps[] = [];
+
+  for (let iTree = 0; iTree < numTrees; iTree += 1) {
+    trees.push({
+      barkColor: theme.tertiaryColor,
+      leafColor: getRandomHexBetweenValues(theme.primaryColor, theme.secondaryColor),
+      ...getRandomCirleOrientation(minDistance, maxDistance),
+    });
+  }
+
+  return trees;
+}
+
+function usePreviousValue<T>(value: T): T  {
+  const ref = useRef(null);
+
+  (ref.current as any) = value;
+
+  useEffect(() => {
+    (ref.current as any) = value;
+  });
+
+  return (ref.current as any);
+}
+
+function getRandomCirleOrientation(minDistance: number, maxDistance: number) {
+  const radius = getRandomInRange(minDistance, maxDistance);
+  const angle = getRandomInRange(0, 2 * Math.PI);
+
+  const posX = Math.sin(angle) * radius;
+  const posZ = Math.cos(angle) * radius;
+
+  const rotation = (angle) / Math.PI * 180;
+
+  return {
+    position: `${posX} 0 ${posZ}`,
+    rotation: `0 ${rotation} 0`,
+  };
+}
+
+export { Forest, LoadForest };
